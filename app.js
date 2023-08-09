0eb7d7831a4d52110580dbaf5f8d14c7aef15457
Rahul Rathore <120238865+rahul-rathore786@users.noreply.github.com>
2023-08-09 21:32:57 +0530

    Update app.js
    Secret page renders only when user is logged in

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 
+// Install requried npm pakage
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const md5 = require("md5");
+// User is Not login
+var isLogin = false;
 
 const homeStartingContent = "A daily blog website is a public platform that enables people to publish their personal blogs, stories, and experiences. It serves as a virtual community of individuals who share their daily lives, offering support, guidance, and inspiration to one another. While maintaining a daily blog on a public website comes with potential risks, the benefits of the platform outweigh the risks for many individuals. It is an excellent resource for aspiring writers, bloggers, and individuals seeking personal growth and connection.";
 const aboutContent = "At our daily blog website, we believe in the power of personal reflection and community support. We understand that maintaining a daily blog can be a challenging task, which is why we have created a platform that enables individuals to connect with like-minded people and share their experiences. Our website provides users with various tools and features to customize their blogs, allowing them to add multimedia content and engage with their audience. We strive to create a safe and supportive environment for our users, where they can be their authentic selves and grow personally and professionally. Join our community today and start your journey of self-reflection and growth.";
@@ -16,11 +21,12 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/blogDB", { useNewUrlParser: true , useUnifiedTopology: true } );
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
 // schema for post collection 
 const postSchema = {
   title: String,
+  name: String,
   content: String
 };
 
@@ -38,15 +44,136 @@ const contactSchema = {
 //Model for contact
 const contactForm = mongoose.model("Contact", contactSchema);
 
+//Create User Schema
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String
 
+
+});
+//Creating a model for the schema and export it so that we can use in our routes file
+const User = mongoose.model("User", userSchema);
+
+// home route
 app.get("/", function (req, res) {
+  res.render("login", { errMsg: "", name: "", password: "" });
+});
 
-  Post.find({}, function (err, posts) {
-    res.render("home", {
-      startingContent: homeStartingContent,
-      posts: posts
-    });
+app.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+app.get("/login", (req, res) => {
+  res.render("login", { errMsg: "", name: "", password: "" });
+});
+
+// Register user
+app.post("/register", function (req, res) {
+  const name = req.body.name
+  const email_by_reg = req.body.email; //Getting Email From The Request Body
+  const password_by_reg = md5(req.body.password); //Getting Password From The Request Body And Hashing It Using MD5
+  //Create A New User
+  const newUser = new User({
+    name: name,
+    email: email_by_reg,
+    password: password_by_reg
+
   });
+
+  //Save The New User To The Database with callback function old way
+
+  // newUser.save(function (err) {
+  //     if (err) {
+  //         console.log(err);
+  //     } else {
+  //         res.render("secrets");
+  //     }
+  // });
+
+  // save the new user to the data base without callback new way
+  newUser.save().then(() => {
+    // res.render("home");
+    isLogin = true;
+    if (isLogin == true) {
+      res.redirect("/secret");
+
+    }
+
+  }).catch((err) => {
+    console.log(err);
+  }
+  );
+
+}
+);
+
+// Login users
+app.post("/login", function (req, res) {
+  const email_by_lg = req.body.email;
+  const password_by_lg = md5(req.body.password);
+
+  // New way supported by new version of mongoose withoutcall back
+
+  User.findOne({
+    email: email_by_lg
+  }).then((foundUser) => {
+
+    if (foundUser) {
+      //If User Is Found Then Check If The Password Is Correct Or Not
+      if (foundUser.password === password_by_lg) {
+        isLogin = true;
+        if (isLogin == true) {
+          res.redirect("/secret");
+        } else {
+          res.redirect("/login");
+        }
+
+      } else {
+        res.render("login", { errMsg: "password incorrect", name: email_by_lg, password: password_by_lg });
+      }
+    } else {
+      res.render("login", { errMsg: "Email does not exists ", name: email_by_lg, password: password_by_lg });
+    }
+  }).catch((err) => {
+    console.log(err);
+  })
+
+  // findOne with callback function old way not supported by new mongoose version
+
+  // User.findOne({
+  //     email: email
+  // }, function (err, foundUser) {
+  //     if (err) {
+  //         console.log(err);
+  //     } else {
+  //         if (foundUser) {
+  //             //If User Is Found Then Check If The Password Is Correct Or Not
+  //             if (foundUser.password === password) {
+  //                 res.render("secrets");
+  //             } else {
+  //                 res.send("Incorrect Password");
+  //             }
+  //         } else {
+  //             res.send("User Not Found");
+  //         }
+  //     }
+  // });
+});
+
+
+app.get("/secret", function (req, res) {
+  if (isLogin == true) {
+    Post.find({}, function (err, posts) {
+      res.render("home", {
+        startingContent: homeStartingContent,
+        posts: posts
+      });
+    });
+  }
+  else {
+    res.redirect("/login");
+  }
 });
 
 app.get("/compose", function (req, res) {
@@ -56,18 +183,19 @@ app.get("/compose", function (req, res) {
 app.post("/compose", function (req, res) {
   const newpost = new Post({
     title: req.body.postTitle,
+    name: req.body.name,
     content: req.body.postBody
   });
 
 
   newpost.save(function (err) {
     if (!err) {
-      res.redirect("/compose");
+      res.redirect("/secret");
     }
   });
 });
 
-app.post("/contactus", function (req, res) { 
+app.post("/contactus", function (req, res) {
   const newcontactForm = new contactForm({
     name: req.body.name,
     email: req.body.email,
@@ -103,6 +231,11 @@ app.get("/about", function (req, res) {
 
 app.get("/contact", function (req, res) {
   res.render("contact", { contactContent: contactContent });
+});
+
+app.get("/logout", function (req, res) {
+  isLogin = false;
+  res.redirect("/login");
 });
 
 
